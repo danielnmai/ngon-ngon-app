99a291d45b9b928be4cdf5d26fb37da888810615
Daniel Mai <danielmai1728@gmail.com>
2025-02-05 13:16:19 -0800

    work on Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,19 +4,12 @@ import classes from "./Hero.module.css";
 export const Hero = () => {
   return (
     <div className={classes.hero}>
-      <Overlay
-        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-        opacity={1}
-        zIndex={0}
-      />
+      <Overlay opacity={0.8} zIndex={0} />
       <Container className={classes.container} size="md">
-        <Title className={classes.title}>
-          A fully featured React components library
-        </Title>
+        <Title className={classes.title}>That Vietnamese Treat</Title>
         <Text className={classes.description} size="xl" mt="xl">
-          Build fully functional accessible web applications faster than ever –
-          Mantine includes more than 120 customizable components and hooks to
-          cover you in any situation
+          The best Vietnamese treats you'll ever have. Nothing more. Nothing
+          less.
         </Text>
 
         <Button
@@ -25,7 +18,7 @@ export const Hero = () => {
           radius="xl"
           className={classes.control}
         >
-          Get started
+          Order Now
         </Button>
       </Container>
     </div>
