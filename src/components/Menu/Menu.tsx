62bd1d69ee2a1e789ad76076626b6fda5ba631fe
Daniel Mai <danielmai1728@gmail.com>
2025-02-17 15:58:43 -0800

    change to use a custom SVG icon

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,35 +1,75 @@
-import { Paper, SimpleGrid, Text, Title } from "@mantine/core";
+import {
+  Container,
+  Paper,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
 import styles from "./Menu.module.css";
 
-const MenuItem = () => {
+import PlusIcon from "../Icons/PlusIcon";
+
+type Food = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+type MenuItemProps = {
+  food: Food;
+};
+
+const MenuItem = ({ food }: MenuItemProps) => {
   return (
-    <Paper className={styles.menuItem}>
-      <Text size="sm">
-        With Fjord Tours you can explore more of the magical fjord landscapes
-        with tours and activities on and around the fjords of Norway
-      </Text>
-    </Paper>
+    <Stack className={styles.menuItemContainer}>
+      <Paper className={styles.menuItemDisplay} radius="lg">
+        <PlusIcon className={styles.menuSelectionIcon} />
+      </Paper>
+      <Title order={4}>{food.name}</Title>
+      <Text c="gray">{food.description}</Text>
+      <Text>{food.price}</Text>
+    </Stack>
   );
 };
 
 const Menu = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/v1/foods");
+        const data = await res.json();
+
+        setFoods(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchFoods();
+  }, []);
+
+  console.log("foods ", foods);
+
   return (
-    <section id="menu">
-      <div className={styles.container}>
+    <section id="menu" className={styles.menuSection}>
+      <Container className={styles.container}>
         <Title className={styles.title} order={1}>
           ORDER ONLINE
         </Title>
         <Title order={3} className={styles.subtitle}>
           We cook fresh for each order, minimum preparation time is 3 hours.
         </Title>
-        <SimpleGrid cols={{ base: 1, sm: 2 }}>
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+          {foods.map((foodItem) => (
+            <MenuItem food={foodItem} />
+          ))}
         </SimpleGrid>
-      </div>
+      </Container>
     </section>
   );
 };
