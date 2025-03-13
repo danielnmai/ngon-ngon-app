ef7b13e7503181507ad35aa98b6b3e6d3b6bac23
Daniel Mai <danielmai1728@gmail.com>
2025-03-12 20:06:52 -0700

    style the order form a bit

--- src/components/OrderModal.tsx
+++ src/components/OrderModal.tsx
@@ -6,6 +6,7 @@ import {
   Radio,
   Stack,
   Text,
+  Textarea,
   Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
@@ -27,6 +28,8 @@ enum Size {
 type FormValues = {
   quantity: number;
   size: Size;
+  foodId: number;
+  specialRequest?: string;
 };
 
 const OrderModal = ({ food, opened, onClose }: ModalProps) => {
@@ -34,6 +37,8 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
     initialValues: {
       quantity: 1,
       size: Size.small,
+      foodId: food.id,
+      specialRequest: "",
     },
   });
 
@@ -51,40 +56,71 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
   };
 
   const handleSubmit = (values: FormValues) => {
+    form.setFieldValue("foodId", food.id);
     console.log("values ", values);
   };
 
   const [quantity, setQuantity] = useState<number>(1);
   return (
-    <Modal opened={opened} onClose={onClose} centered>
+    <Modal opened={opened} onClose={onClose} centered size="md">
       <Stack>
         <Title order={3}>{food.name}</Title>
         <Text>{food.description}</Text>
         <Image src="src/assets/pictures/bun-dau-mam-tom.jpg" />
       </Stack>
       <form onSubmit={form.onSubmit(handleSubmit)}>
-        <Radio.Group {...form.getInputProps("size")} key={form.key("size")}>
-          <Radio value={Size.small} label="Small" />
-          <Radio value={Size.medium} label="Medium" />
-          <Radio value={Size.large} label="Large" />
+        <Radio.Group
+          label="Size"
+          {...form.getInputProps("size")}
+          key={form.key("size")}
+          my={10}
+        >
+          <Radio
+            value={Size.small}
+            label="Small"
+            my={8}
+            classNames={{
+              label: "ml-2",
+            }}
+          />
+          <Radio
+            value={Size.medium}
+            label="Medium"
+            my={8}
+            classNames={{
+              label: "ml-2",
+            }}
+          />
+          <Radio
+            className="my-5"
+            value={Size.large}
+            label="Large"
+            my={8}
+            classNames={{
+              label: "ml-2",
+            }}
+          />
         </Radio.Group>
 
-        <Group w="100%" justify="space-between" mt={5}>
+        <Textarea
+          label="Special Request"
+          placeholder="More salty? Extra sauce? We'll try our best to accomodate."
+          my={10}
+          key={form.key("specialRequest")}
+          {...form.getInputProps("specialRequest")}
+        />
+
+        <Group w="100%" justify="space-between" m={10} className="select-none">
           <Group w="40%" justify="space-between">
-            <MinusIcon
-              onClick={decrementQuantity}
-              cursor="pointer"
-              width={30}
-              height={30}
-            />
+            <div onClick={decrementQuantity} className="cursor-pointer p-2">
+              <MinusIcon width={30} height={30} />
+            </div>
             <div className="select-none">{quantity}</div>
-            <PlusIcon
-              onClick={incrementQuantity}
-              cursor="pointer"
-              width={30}
-              height={30}
-            />
+            <div onClick={incrementQuantity} className="cursor-pointer p-2">
+              <PlusIcon cursor="pointer" width={30} height={30} />
+            </div>
           </Group>
+
           <Button type="submit">Add to Cart</Button>
         </Group>
       </form>
