02309786d94fcb2c5d0898be3eea0f20a3d5152e
Daniel Mai <danielmai1728@gmail.com>
2025-03-14 10:55:26 -0700

    style the quantity and submit button row of order modal

--- src/components/OrderModal.tsx
+++ src/components/OrderModal.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Container,
   Group,
   Image,
   Modal,
@@ -62,68 +63,92 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
 
   const [quantity, setQuantity] = useState<number>(1);
   return (
-    <Modal opened={opened} onClose={onClose} centered size="md">
-      <Stack>
-        <Title order={3}>{food.name}</Title>
-        <Text>{food.description}</Text>
-        <Image src="src/assets/pictures/bun-dau-mam-tom.jpg" />
-      </Stack>
-      <form onSubmit={form.onSubmit(handleSubmit)}>
-        <Radio.Group
-          label="Size"
-          {...form.getInputProps("size")}
-          key={form.key("size")}
-          my={10}
-        >
-          <Radio
-            value={Size.small}
-            label="Small"
-            my={8}
-            classNames={{
-              label: "ml-2",
-            }}
-          />
-          <Radio
-            value={Size.medium}
-            label="Medium"
-            my={8}
-            classNames={{
-              label: "ml-2",
-            }}
-          />
-          <Radio
-            className="my-5"
-            value={Size.large}
-            label="Large"
-            my={8}
-            classNames={{
-              label: "ml-2",
-            }}
-          />
-        </Radio.Group>
+    <Modal opened={opened} onClose={onClose} centered size="lg">
+      <Container my={8}>
+        <Stack>
+          <Title order={3}>{food.name}</Title>
+          <Text>{food.description}</Text>
+          <Image src="src/assets/pictures/bun-dau-mam-tom.jpg" />
+        </Stack>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Radio.Group
+            label="Size"
+            {...form.getInputProps("size")}
+            key={form.key("size")}
+            my={10}
+          >
+            <Radio
+              value={Size.small}
+              label="Small"
+              my={8}
+              color="var(--color-primary)"
+              classNames={{
+                label: "ml-2",
+              }}
+            />
+            <Radio
+              value={Size.medium}
+              label="Medium"
+              my={8}
+              color="var(--color-primary)"
+              classNames={{
+                label: "ml-2",
+              }}
+            />
+            <Radio
+              className="my-5"
+              value={Size.large}
+              label="Large"
+              my={8}
+              color="var(--color-primary)"
+              classNames={{
+                label: "ml-2",
+              }}
+            />
+          </Radio.Group>
 
-        <Textarea
-          label="Special Request"
-          placeholder="More salty? Extra sauce? We'll try our best to accomodate."
-          my={10}
-          key={form.key("specialRequest")}
-          {...form.getInputProps("specialRequest")}
-        />
+          <Textarea
+            label="Special Request"
+            placeholder="More salty? Extra sauce? We'll try our best to accomodate."
+            className="my-4"
+            key={form.key("specialRequest")}
+            {...form.getInputProps("specialRequest")}
+            classNames={{
+              label: "mb-2",
+              input: "focus:border-primary",
+            }}
+          />
 
-        <Group w="100%" justify="space-between" m={10} className="select-none">
-          <Group w="40%" justify="space-between">
-            <div onClick={decrementQuantity} className="cursor-pointer p-2">
-              <MinusIcon width={30} height={30} />
-            </div>
-            <div className="select-none">{quantity}</div>
-            <div onClick={incrementQuantity} className="cursor-pointer p-2">
-              <PlusIcon cursor="pointer" width={30} height={30} />
-            </div>
+          <Group w="100%" className="justify-between flex-nowrap">
+            <Group className="max-w-[200px] select-none flex-nowrap ">
+              <div
+                onClick={decrementQuantity}
+                className="cursor-pointer p-2 hover:bg-gray-100 hover:rounded-md"
+              >
+                <MinusIcon width={30} height={30} />
+              </div>
+              <input
+                className="w-12 p-2 text-center focus:outline-none focus:border-primary focus:rounded-md focus:border-solid focus:border-1 "
+                aria-label="order quantity input"
+                key={form.key("quantity")}
+                maxLength={3}
+                {...form.getInputProps("quantity")}
+              />
+
+              <div
+                onClick={incrementQuantity}
+                className="cursor-pointer p-2 hover:bg-gray-100 hover:rounded-md"
+              >
+                <PlusIcon cursor="pointer" width={30} height={30} />
+              </div>
+            </Group>
+
+            <Button size="md" className="bg-primary" type="submit">
+              Add to Cart
+            </Button>
           </Group>
-
-          <Button type="submit">Add to Cart</Button>
-        </Group>
-      </form>
+        </form>
+      </Container>
     </Modal>
   );
 };
