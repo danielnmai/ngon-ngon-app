6d72997e0981efab2c3274fb75d675d32a096f33
Daniel Mai <danielmai1728@gmail.com>
2025-03-19 15:11:19 -0700

    add logic to display item total in the order modal form

--- src/components/OrderModal.tsx
+++ src/components/OrderModal.tsx
@@ -4,6 +4,7 @@ import {
   Group,
   Image,
   Modal,
+  NumberFormatter,
   Radio,
   Stack,
   Text,
@@ -12,8 +13,9 @@ import {
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { MinusIcon, PlusIcon } from "lucide-react";
-import { useState } from "react";
-import { Food } from "../schemas/menu";
+import { ChangeEvent, useState } from "react";
+import { Food, FoodOption } from "../schemas/menu";
+
 type ModalProps = {
   food: Food;
   opened: boolean;
@@ -21,9 +23,9 @@ type ModalProps = {
 };
 
 enum Size {
-  small = "small",
-  medium = "medium",
-  large = "large",
+  small = "SMALL",
+  medium = "MEDIUM",
+  large = "LARGE",
 }
 
 type FormValues = {
@@ -31,37 +33,74 @@ type FormValues = {
   size: Size;
   foodId: number;
   specialRequest?: string;
+  totalPrice: number;
 };
 
 const OrderModal = ({ food, opened, onClose }: ModalProps) => {
+  const [quantity, setQuantity] = useState<number>(food.options[0].minQuantity);
+  const [totalPrice, setTotalPrice] = useState<number>(food.options[0].price);
+  const [selectedOption, setSelectedOption] = useState<FoodOption>(
+    food.options[0]
+  );
+
   const form = useForm<FormValues>({
     initialValues: {
-      quantity: 1,
-      size: Size.small,
+      quantity: food.options[0].minQuantity,
+      size: Size.medium,
       foodId: food.id,
       specialRequest: "",
+      totalPrice: food.options[0].price,
     },
   });
 
   const incrementQuantity = () => {
     setQuantity(quantity + 1);
     form.setFieldValue("quantity", quantity + 1);
+    setTotalPrice(totalPrice + selectedOption.price);
+    form.setFieldValue("totalPrice", totalPrice + selectedOption.price);
   };
 
   const decrementQuantity = () => {
-    if (quantity == 1) {
+    if (quantity == food.options[0].minQuantity) {
       return;
     }
     setQuantity(quantity - 1);
     form.setFieldValue("quantity", quantity - 1);
+    setTotalPrice(totalPrice - selectedOption!.price);
+    form.setFieldValue("totalPrice", totalPrice - selectedOption.price);
   };
 
   const handleSubmit = (values: FormValues) => {
-    form.setFieldValue("foodId", food.id);
-    console.log("values ", values);
+    console.log(values);
   };
 
-  const [quantity, setQuantity] = useState<number>(1);
+  const onRadioOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const option = food.options.find((option) => option.size === value);
+
+    if (option) {
+      setSelectedOption(option);
+      setTotalPrice(option.price * quantity);
+      form.setFieldValue("totalPrice", option.price * quantity);
+    }
+  };
+
+  const renderFoodOption = (option: FoodOption) => {
+    return (
+      <Radio
+        key={option.id}
+        value={option.size}
+        label={option.size}
+        onChange={onRadioOptionChange}
+        my={8}
+        color="var(--color-primary)"
+        classNames={{
+          label: "ml-2",
+        }}
+      />
+    );
+  };
+
   return (
     <Modal opened={opened} onClose={onClose} centered size="lg">
       <Container my={8}>
@@ -77,34 +116,7 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
             key={form.key("size")}
             my={10}
           >
-            <Radio
-              value={Size.small}
-              label="Small"
-              my={8}
-              color="var(--color-primary)"
-              classNames={{
-                label: "ml-2",
-              }}
-            />
-            <Radio
-              value={Size.medium}
-              label="Medium"
-              my={8}
-              color="var(--color-primary)"
-              classNames={{
-                label: "ml-2",
-              }}
-            />
-            <Radio
-              className="my-5"
-              value={Size.large}
-              label="Large"
-              my={8}
-              color="var(--color-primary)"
-              classNames={{
-                label: "ml-2",
-              }}
-            />
+            {food.options.map(renderFoodOption)}
           </Radio.Group>
 
           <Textarea
@@ -128,11 +140,29 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
                 <MinusIcon width={30} height={30} />
               </div>
               <input
+                type="text"
+                inputMode="numeric"
+                value={quantity}
                 className="w-12 p-2 text-center focus:outline-none focus:border-primary focus:rounded-md focus:border-solid focus:border-1 "
                 aria-label="order quantity input"
                 key={form.key("quantity")}
                 maxLength={3}
                 {...form.getInputProps("quantity")}
+                onChange={(e) => {
+                  if (!e.target.value) {
+                    setQuantity(food.options[0].minQuantity);
+                    form.setFieldValue("quantity", food.options[0].minQuantity);
+                    return;
+                  }
+                  const quantity = parseInt(e.target.value);
+                  setQuantity(quantity);
+                  form.setFieldValue("quantity", quantity);
+                  setTotalPrice(quantity * selectedOption.price);
+                  form.setFieldValue(
+                    "totalPrice",
+                    quantity * selectedOption.price
+                  );
+                }}
               />
 
               <div
@@ -144,7 +174,14 @@ const OrderModal = ({ food, opened, onClose }: ModalProps) => {
             </Group>
 
             <Button size="md" className="bg-primary" type="submit">
-              Add to Cart
+              Add to Cart -
+              <NumberFormatter
+                prefix=" $"
+                value={totalPrice / 100}
+                thousandSeparator
+                decimalScale={2}
+                fixedDecimalScale
+              />
             </Button>
           </Group>
         </form>
--- src/schemas/menu.ts
+++ src/schemas/menu.ts
@@ -1,11 +1,21 @@
 import { z } from "zod";
 
+const FoodOptionSchema = z.object({
+  id: z.number(),
+  size: z.enum(["SMALL", "MEDIUM", "LARGE"]),
+  price: z.number(),
+  minQuantity: z.number(),
+  foodId: z.number(),
+});
+
 const FoodSchema = z.object({
   id: z.number(),
   name: z.string(),
   description: z.string(),
   price: z.number(),
+  options: z.array(FoodOptionSchema),
 });
 
 export type Food = z.infer<typeof FoodSchema>;
+export type FoodOption = z.infer<typeof FoodOptionSchema>;
 export default FoodSchema;
