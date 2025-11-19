1ac7963778e7874106e5664905433c82b2148023
Daniel Mai <danielmai1728@gmail.com>
2025-11-18 18:52:35 -0800

    configure env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,7 @@ import type { Food } from "../schemas/menu";
 import type { Order, OrderResponse } from "../schemas/order";
 
 class APIService {
-	// BASE_URL = "https://dominque-unmonumented-jenee.ngrok-free.dev/v1";
-	BASE_URL = "http://localhost:3000/v1";
+	BASE_URL = import.meta.env.VITE_API_URL;
 	axiosInstance: AxiosInstance;
 
 	constructor() {
